4e77d4756b8cd98dbf5f74f597b3173b6b896b0d
jmittler <jmittler@uw.edu>
2019-02-26 11:52:33 -0800

    Add files via upload

---
+++ Figure5/ParamsFig5.R
@@ -0,0 +1,21 @@
+# Assume that success in graduate school is the the sum of up to eight variables
+Sample_size <- 10000
+Admissions_Cutoff <- 0.80 # Overall rank to get accepted into graduate program
+Attends_Harvard_Instead <- 0.87  # Overall rank (using Harvard's criteria) above which students go elsewhere
+Weight_Irrelevant_Factor <- 1.0
+show_gre_gpa_correlations <- FALSE
+new_simulation_data <- 1
+key_graphs_only <- TRUE
+
+# Contributions (s1, s2,...) to success in graduate school / Weights (w1, w2, ...) used by admissions committee and Harvard's admissions committee
+s1 <- 9.0; w1 <- 15.0; h1 <- 15.0; # GRE  
+s2 <- 1.0; w2 <- 3.0; h2 <- 3.0; # GPA                      
+s3 <- 1.0; w3 <- 1.0; h3 <- 1.0; # Research experience      
+s4 <- 2.0; w4 <- 2.0; h4 <- 2.0; # Ability to work in lab (as assessed by LORs)
+s5 <- 1.0; w5 <- 1.0; h5 <- 1.0; # Subtle / unquantifable factors correctly intuited by committee members  
+s6 <- 0.0; w6 <- 1.0; h6 <- 1.0; # Irrelevant factors used by committee members
+s7 <- 0.0; w7 <- 0.0; h7 <- 0.0; # Irrelevant factors used Harvard's admissions committee
+s8 <- 4.0; w8 <- 0.0; h8 <- 0.0; # Chance effects
+
+Corr12 <- 0.2 # Correlation between GRE and GPA
+Corr34 <- 0.2 # Correlation between research experience and LORs
